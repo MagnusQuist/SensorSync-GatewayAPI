e57756b10ffec701b0efebd1698c1b9ecc324d2b
Rasmus Helleberg Eriksen <raeri21@student.sdu.dk>
2024-04-08 16:23:46 +0200

    Security yes

---
+++ src/main/java/dk/sdu/apigateway/security/AuthFilter.java
@@ -0,0 +1,77 @@
+package dk.sdu.apigateway.security;
+
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@RefreshScope
+@Component
+public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> {
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Value("${jwt.prefix}")
+    public String TOKEN_PREFIX;
+
+    public AuthFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+
+            if (this.isAuthMissing(request) || this.isPrefixMissing(request))
+                return this.onError(exchange, "Authorization header is missing in request");
+
+            final String token = this.getAuthHeader(request);
+
+            if (jwtUtil.isInvalid(token))
+                return this.onError(exchange, "Authorization header is invalid");
+
+            this.populateRequestWithHeaders(exchange, token);
+
+            return chain.filter(exchange);
+        };
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange, String err) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(HttpStatus.UNAUTHORIZED);
+        return response.setComplete();
+    }
+
+    private String getAuthHeader(ServerHttpRequest request) {
+        var header = request.getHeaders().getOrEmpty("Authorization").get(0);
+        return header.replace(TOKEN_PREFIX,"").trim();
+    }
+
+    private boolean isAuthMissing(ServerHttpRequest request) {
+        return !request.getHeaders().containsKey("Authorization");
+    }
+
+    private boolean isPrefixMissing(ServerHttpRequest request) {
+        var header = request.getHeaders().getFirst ("Authorization");
+        assert header != null;
+        return !header.startsWith(TOKEN_PREFIX);
+    }
+
+    private void populateRequestWithHeaders(ServerWebExchange exchange, String token) {
+        Claims claims = jwtUtil.getAllClaimsFromToken(token);
+        exchange.getRequest().mutate().build();
+    }
+
+    public static class Config {
+
+    }
+}
